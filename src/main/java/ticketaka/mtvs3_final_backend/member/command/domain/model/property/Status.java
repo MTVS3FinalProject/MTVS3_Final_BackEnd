bd7357dbe32a80cb5d52fd6ff088961b8071b1d1
INUK-ai <arheon2469@gmail.com>
2024-10-09 23:30:45 +0900

    Feat: Status

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/model/property/Status.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/model/property/Status.java
@@ -1,4 +1,12 @@
 package ticketaka.mtvs3_final_backend.member.command.domain.model.property;
 
+import lombok.Getter;
+import ticketaka.mtvs3_final_backend._core.error.exception.Exception400;
+
 public enum Status {
+    // 활성, 휴면, 탈퇴, 정지
+    ACTIVE,
+    DORMANT,
+    DEACTIVATED,
+    SUSPENDED
 }
