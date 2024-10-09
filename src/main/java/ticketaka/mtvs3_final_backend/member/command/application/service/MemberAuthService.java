c907c876f317d129769bc682df3711d7b7308efd
INUK-ai <arheon2469@gmail.com>
2024-10-09 23:49:21 +0900

    Feat: MemberAuthService - newMember

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/application/service/MemberAuthService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/application/service/MemberAuthService.java
@@ -2,9 +2,19 @@ package ticketaka.mtvs3_final_backend.member.command.application.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend._core.error.exception.Exception400;
+import ticketaka.mtvs3_final_backend.member.command.application.dto.MemberAuthRequestDTO;
+import ticketaka.mtvs3_final_backend.member.command.domain.model.Member;
+import ticketaka.mtvs3_final_backend.member.command.domain.model.property.AgeGroup;
+import ticketaka.mtvs3_final_backend.member.command.domain.model.property.Authority;
+import ticketaka.mtvs3_final_backend.member.command.domain.model.property.Gender;
+import ticketaka.mtvs3_final_backend.member.command.domain.model.property.Status;
 import ticketaka.mtvs3_final_backend.member.command.domain.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -13,4 +23,48 @@ import ticketaka.mtvs3_final_backend.member.command.domain.repository.MemberRepo
 public class MemberAuthService {
 
     private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    /*
+        기본 회원 가입
+     */
+    @Transactional
+    public void signUp(MemberAuthRequestDTO.signUpDTO requestDTO) {
+
+        // 이메일 중복 확인
+        checkDuplicatedEmail(requestDTO.email());
+
+        // 비밀번호 확인
+        checkValidPassword(requestDTO.password(), passwordEncoder.encode(requestDTO.confirmPassword()));
+
+        // 회원 생성
+        Member member = newMember(requestDTO);
+
+        // 회원 저장
+        memberRepository.save(member);
+    }
+
+    // 이메일 중복 확인
+    private void checkDuplicatedEmail(String email) {
+
+        Optional<Member> member = memberRepository.findByEmail(email);
+
+        if(member.isPresent()) {
+            throw new Exception400("이미 가입된 이메일입니다.");
+        }
+    }
+
+    // 회원 생성
+    protected Member newMember(MemberAuthRequestDTO.signUpDTO requestDTO) {
+        return Member.builder()
+                .nickname(requestDTO.nickName())
+                .email(requestDTO.email())
+                .password(passwordEncoder.encode(requestDTO.password()))
+                .gender(Gender.fromString(requestDTO.gender()))
+                .ageGroup(AgeGroup.fromString(requestDTO.age_range()))
+                .authority(Authority.USER)
+                .status(Status.ACTIVE)
+                .build();
+    }
 }
