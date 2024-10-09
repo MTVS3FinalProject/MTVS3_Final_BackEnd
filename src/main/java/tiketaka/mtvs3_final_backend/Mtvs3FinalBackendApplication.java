1b0f62a16239d551585917c034c81575570db84d
INUK-ai <arheon2469@gmail.com>
2024-10-09 21:45:14 +0900

    Setting: Init Project

---
+++ src/main/java/tiketaka/mtvs3_final_backend/Mtvs3FinalBackendApplication.java
@@ -0,0 +1,13 @@
+package tiketaka.mtvs3_final_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mtvs3FinalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mtvs3FinalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tiketaka/mtvs3_final_backend/Mtvs3FinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tiketaka.mtvs3_final_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mtvs3FinalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
