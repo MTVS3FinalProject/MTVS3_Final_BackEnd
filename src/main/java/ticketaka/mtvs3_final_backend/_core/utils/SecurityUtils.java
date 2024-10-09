af5e7971ab67fb069223fa1a4accfd9df2464da7
INUK-ai <arheon2469@gmail.com>
2024-10-09 22:48:59 +0900

    Feat: SecurityUtils

---
+++ src/main/java/ticketaka/mtvs3_final_backend/_core/utils/SecurityUtils.java
@@ -0,0 +1,18 @@
+package ticketaka.mtvs3_final_backend._core.utils;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import ticketaka.mtvs3_final_backend._core.error.exception.Exception401;
+
+public class SecurityUtils {
+
+    public static Long getCurrentMemberId() {
+
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        if(name.equals("anonymousUser")) {
+            throw new Exception401("해당 유저의 ID를 찾을 수 없습니다.");
+        }
+
+        return Long.parseLong(name);
+    }
+}
