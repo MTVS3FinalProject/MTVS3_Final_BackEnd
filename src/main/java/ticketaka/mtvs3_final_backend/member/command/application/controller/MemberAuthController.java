8a8d100bc42e813373370f05f75ab0021a030a54
INUK-ai <arheon2469@gmail.com>
2024-10-09 23:56:37 +0900

    Feat: MemberAuthController - login

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/application/controller/MemberAuthController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/application/controller/MemberAuthController.java
@@ -1,8 +1,10 @@
 package ticketaka.mtvs3_final_backend.member.command.application.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
 import ticketaka.mtvs3_final_backend.member.command.application.dto.MemberAuthRequestDTO;
+import ticketaka.mtvs3_final_backend.member.command.application.dto.MemberAuthResponseDTO;
 import ticketaka.mtvs3_final_backend.member.command.application.service.MemberAuthService;
 
 @Slf4j
@@ -29,5 +32,19 @@ public class MemberAuthController {
         memberAuthService.signUp(requestDTO);
 
         return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
+
+    /*
+        기본 로그인
+     */
+    @PostMapping("/login")
+    public ResponseEntity<?> login(HttpServletRequest httpServletRequest, @Valid @RequestBody MemberAuthRequestDTO.authDTO requestDTO) {
+
+        MemberAuthResponseDTO.authTokenDTO responseDTO = memberAuthService.login(httpServletRequest, requestDTO);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, responseDTO.grantType() + " " + responseDTO.accessToken())
+                .header("Refresh-Token", responseDTO.grantType() + " " + responseDTO.refreshToken())
+                .body(ApiUtils.success(null));
     }
 }
