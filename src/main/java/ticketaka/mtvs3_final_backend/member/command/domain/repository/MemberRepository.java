8a471c9c5d21a48784e3e6b86336e8f43691251e
INUK-ai <arheon2469@gmail.com>
2024-10-09 23:44:24 +0900

    Feat: MemberRepository - findByEmail

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/repository/MemberRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package ticketaka.mtvs3_final_backend.member.command.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ticketaka.mtvs3_final_backend.member.command.domain.model.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
