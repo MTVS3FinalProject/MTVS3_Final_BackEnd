c6cb8817418a9ac280298e9fee8cfe3408d4455f
INUK-ai <arheon2469@gmail.com>
2024-10-09 23:47:01 +0900

    Feat: MemberAuthRequestDTO - signUpDTO

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/application/controller/MemberAuthController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/application/controller/MemberAuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
+import ticketaka.mtvs3_final_backend.member.command.application.dto.MemberAuthRequestDTO;
 import ticketaka.mtvs3_final_backend.member.command.application.service.MemberAuthService;
 
 @Slf4j
