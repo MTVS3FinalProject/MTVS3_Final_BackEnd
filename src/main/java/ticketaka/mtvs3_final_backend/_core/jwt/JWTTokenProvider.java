5d1a3b7042ff8f61fc229bb53897c988af023073
INUK-ai <arheon2469@gmail.com>
2024-10-09 22:37:39 +0900

    Feat: MemberAuthResponseDTO - authTokenDTO

--- src/main/java/ticketaka/mtvs3_final_backend/_core/jwt/JWTTokenProvider.java
+++ src/main/java/ticketaka/mtvs3_final_backend/_core/jwt/JWTTokenProvider.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import ticketaka.mtvs3_final_backend.member.command.application.dto.MemberAuthResponseDTO;
 
 import java.security.Key;
 import java.util.Arrays;
