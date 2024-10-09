0350e11ba8ed6b0608bf9fdb840cc3da1da42513
INUK-ai <arheon2469@gmail.com>
2024-10-09 23:15:57 +0900

    Feat: Member Entity

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/model/Member.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/model/Member.java
@@ -1,4 +1,51 @@
 package ticketaka.mtvs3_final_backend.member.command.domain.model;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import ticketaka.mtvs3_final_backend.BaseTimeEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member_tb")
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nickname;
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+    @Column
+    private AgeGroup ageGroup;
+    @Column
+    private Gender gender;
+    @Column
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'USER'")
+    private Authority authority;
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    @ColumnDefault("'ACTIVE'")
+    private Status status;
+
+    @Builder
+    public Member(String nickname, String email, String password, Gender gender, AgeGroup ageGroup, Authority authority, Status status) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.ageGroup = ageGroup;
+        this.authority = authority;
+        this.status = status;
+    }
 }
