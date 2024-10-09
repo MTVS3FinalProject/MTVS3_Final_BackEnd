e5e8e8f3cb3a4607d14d215e6df86d60e4463030
INUK-ai <arheon2469@gmail.com>
2024-10-09 22:34:34 +0900

    Setting: GlobalExceptionHandler

--- src/main/java/ticketaka/mtvs3_final_backend/_core/error/GlobalExceptionHandler.java
+++ src/main/java/ticketaka/mtvs3_final_backend/_core/error/GlobalExceptionHandler.java
@@ -1,4 +1,55 @@
 package ticketaka.mtvs3_final_backend._core.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ticketaka.mtvs3_final_backend._core.error.exception.*;
+import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception400.class)
+    public ResponseEntity<?> badRequest(Exception400 e){
+        return new ResponseEntity<>(e.body(), e.status());
+    }
+
+    @ExceptionHandler(Exception401.class)
+    public ResponseEntity<?> unAuthorized(Exception401 e){
+        return new ResponseEntity<>(e.body(), e.status());
+    }
+
+    @ExceptionHandler(Exception403.class)
+    public ResponseEntity<?> forbidden(Exception403 e){
+        return new ResponseEntity<>(e.body(), e.status());
+    }
+
+    @ExceptionHandler(Exception404.class)
+    public ResponseEntity<?> notFound(Exception404 e){
+        return new ResponseEntity<>(e.body(), e.status());
+    }
+
+    @ExceptionHandler(Exception500.class)
+    public ResponseEntity<?> serverError(Exception500 e){
+        return new ResponseEntity<>(e.body(), e.status());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        // errors 맵의 첫 번째 요소의 값을 가져옴
+        String firstErrorMessage = errors.values().stream().findFirst().orElse("유효성 검사 오류가 발생했습니다.");
+
+        return new ResponseEntity<>(ApiUtils.error(firstErrorMessage, HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+    }
 }
