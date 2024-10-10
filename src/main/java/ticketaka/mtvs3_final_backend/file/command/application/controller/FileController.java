798190f59f596e773c4bf8f584c06908b8fd6105
INUK-ai <arheon2469@gmail.com>
2024-10-10 15:20:27 +0900

    Feat: FileController - uploadImg

--- src/main/java/ticketaka/mtvs3_final_backend/file/command/application/controller/FileController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/controller/FileController.java
@@ -2,9 +2,14 @@ package ticketaka.mtvs3_final_backend.file.command.application.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
 import ticketaka.mtvs3_final_backend.file.command.application.service.FileService;
+
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,4 +18,17 @@ import ticketaka.mtvs3_final_backend.file.command.application.service.FileServic
 public class FileController {
 
     private final FileService fileService;
+
+    /*
+        파일 업로드 테스트
+     */
+    public ResponseEntity<?> uploadImg(MultipartFile file) throws IOException {
+
+        String fileName = file.getOriginalFilename();
+        String fileUrl = fileService.uploadFirebaseBucket(file, fileName);
+
+        log.info("File Url : {}", fileUrl);
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
 }
