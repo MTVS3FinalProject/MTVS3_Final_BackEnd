a9f4e976a13380f5722872d619e061cdd40ba673
INUK-ai <arheon2469@gmail.com>
2024-10-10 15:11:01 +0900

    Feat: FileService - uploadFirebaseBucket

--- src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/FileService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/FileService.java
@@ -1,10 +1,16 @@
 package ticketaka.mtvs3_final_backend.file.command.application.service;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.cloud.StorageClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,4 +20,14 @@ public class FileService {
 
     @Value("${FIREBASE.STORAGE}")
     private String firebaseStorageUrl;
+
+    // 파일 업로드
+    public String uploadFirebaseBucket(MultipartFile multipartFile, String fileName) throws IOException {
+        Bucket bucket = StorageClient.getInstance().bucket(firebaseStorageUrl);
+
+        Blob blob = bucket.create(fileName,
+                multipartFile.getInputStream(), multipartFile.getContentType());
+
+        return blob.getMediaLink(); // 파이어베이스에 저장된 파일 url
+    }
 }
