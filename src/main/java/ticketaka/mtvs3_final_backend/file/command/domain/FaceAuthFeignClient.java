d8b6e17d824ca90e5e55f2ddde79e997bacbd88c
INUK-ai <arheon2469@gmail.com>
2024-10-10 10:22:25 +0900

    Feat: FaceAuthFeignClient - identifyFaceAuth

--- src/main/java/ticketaka/mtvs3_final_backend/file/command/domain/FaceAuthFeignClient.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/domain/FaceAuthFeignClient.java
@@ -1,8 +1,12 @@
 package ticketaka.mtvs3_final_backend.file.command.domain;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "face_auth_service", url = "https://adapted-charmed-panda.ngrok-free.app")
 public interface FaceAuthFeignClient {
 
+    @PostMapping("/verification")
+    void identifyFaceAuth(@RequestBody String check);
 }
