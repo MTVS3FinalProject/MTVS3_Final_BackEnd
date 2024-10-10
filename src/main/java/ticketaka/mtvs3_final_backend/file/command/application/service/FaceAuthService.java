b95d6944c2e846a34d81a5d92c23480a1540ba34
INUK-ai <arheon2469@gmail.com>
2024-10-10 13:23:00 +0900

    Feat: FaceAuthService - identifyFace

--- src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/FaceAuthService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/FaceAuthService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend._core.error.exception.Exception401;
+import ticketaka.mtvs3_final_backend.file.command.application.dto.FaceAuthRequestDTO;
+import ticketaka.mtvs3_final_backend.file.command.application.dto.FaceAuthResponseDTO;
 import ticketaka.mtvs3_final_backend.file.command.domain.service.FaceAuthFeignClient;
 
 @Slf4j
@@ -13,4 +16,29 @@ import ticketaka.mtvs3_final_backend.file.command.domain.service.FaceAuthFeignCl
 public class FaceAuthService {
 
     private final FaceAuthFeignClient faceAuthFeignClient;
+
+    /*
+        얼굴 인식
+     */
+    public void identifyFace(FaceAuthRequestDTO.getCurrentFaceImgDTO requestDTO) {
+
+        // 유저 이미지 조회
+        String originImgUrl = "";
+
+        // FeignRequestDTO 생성
+        FaceAuthRequestDTO.identifyFaceDTO feignRequestDTO = new FaceAuthRequestDTO.identifyFaceDTO(
+                originImgUrl,
+                requestDTO.imgUrl()
+        );
+
+        // AI 통신
+        FaceAuthResponseDTO.identifyFaceDTO responseDTO = faceAuthFeignClient.identifyFace(feignRequestDTO);
+
+        log.info("{}", responseDTO);
+
+        // 결과 확인
+        if (!responseDTO.match_result()) {
+            throw new Exception401("얼굴 인식에 실패하였습니다.");
+        }
+    }
 }
