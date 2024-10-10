9b575a22f2013a45bf2f020cd7ab35b1a79bef3b
INUK-ai <arheon2469@gmail.com>
2024-10-10 11:07:51 +0900

    Feat: FaceAuthController

---
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/controller/FaceAuthController.java
@@ -0,0 +1,17 @@
+package ticketaka.mtvs3_final_backend.file.command.application.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/face")
+public class FaceAuthController {
+
+    private final FaceAuthService faceAuthService;
+
+
+}
