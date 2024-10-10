29208aafcdbf55237bff5c9c420d964b4e5c5ea5
INUK-ai <arheon2469@gmail.com>
2024-10-10 21:58:14 +0900

    Refactor: FileController - uploadImg

--- src/main/java/ticketaka/mtvs3_final_backend/file/command/application/controller/FileController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/controller/FileController.java
@@ -3,13 +3,9 @@ package ticketaka.mtvs3_final_backend.file.command.application.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
-import ticketaka.mtvs3_final_backend.file.command.application.dto.FileRequestDTO;
 import ticketaka.mtvs3_final_backend.file.command.application.service.FileService;
 
 import java.io.IOException;
@@ -26,9 +22,8 @@ public class FileController {
         파일 업로드 테스트
      */
     @PostMapping
-    public ResponseEntity<?> uploadImg(@RequestBody FileRequestDTO.uploadUserImgDTO requestDTO) throws IOException {
+    public ResponseEntity<?> uploadImg(@RequestParam("image") MultipartFile image) throws IOException {
 
-        MultipartFile image = requestDTO.image();
         fileService.uploadFirebaseBucket(image, image.getOriginalFilename());
 
         return ResponseEntity.ok().body(ApiUtils.success(null));
