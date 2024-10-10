4a8e5ac7ebe3f04329d236f60af7bd801aac154b
INUK-ai <arheon2469@gmail.com>
2024-10-10 11:24:53 +0900

    Refactor: FaceAuthService

--- src/main/java/ticketaka/mtvs3_final_backend/Mtvs3FinalBackendApplication.java
+++ src/main/java/ticketaka/mtvs3_final_backend/Mtvs3FinalBackendApplication.java
@@ -2,8 +2,10 @@ package ticketaka.mtvs3_final_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Mtvs3FinalBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/FaceAuthService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/FaceAuthService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.file.command.domain.service.FaceAuthFeignClient;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -11,4 +12,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class FaceAuthService {
 
+    private final FaceAuthFeignClient faceAuthFeignClient;
 }
