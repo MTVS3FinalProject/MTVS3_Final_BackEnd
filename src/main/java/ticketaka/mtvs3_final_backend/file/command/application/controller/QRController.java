ad3f53f00bf9ce75456a177abd2c0e128a6f36ec
INUK-ai <arheon2469@gmail.com>
2024-10-11 17:05:28 +0900

    Refactor: QRService - generateSignUpQR

--- src/main/java/ticketaka/mtvs3_final_backend/file/command/application/controller/QRController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/controller/QRController.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ticketaka.mtvs3_final_backend.file.command.application.dto.QRRequestDTO;
 import ticketaka.mtvs3_final_backend.file.command.application.service.QRService;
 
 @Slf4j
@@ -18,9 +20,9 @@ public class QRController {
     private final QRService qrService;
 
     @GetMapping(value = "/signup", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<?> generateSignUpQR() {
+    public ResponseEntity<?> generateSignUpQR(@RequestBody QRRequestDTO.generateSignUpQRDTO requestDTO) {
 
-        byte[] responseDTO = qrService.generateSignUpQR();
+        byte[] responseDTO = qrService.generateSignUpQR(requestDTO);
 
         return ResponseEntity.ok()
                 .contentType(MediaType.IMAGE_PNG)
--- src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/QRService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/command/application/service/QRService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.file.command.application.dto.QRRequestDTO;
+import ticketaka.mtvs3_final_backend.redis.identification.domain.Identification;
+import ticketaka.mtvs3_final_backend.redis.identification.repository.IdentificationRedisRepository;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -19,22 +22,44 @@ import java.io.IOException;
 @Service
 public class QRService {
 
+    private final IdentificationRedisRepository identificationRedisRepository;
+
     private static final int QR_WIDTH = 200;
     private static final int QR_HEIGHT = 200;
+    private static final String QR_FORMAT = "PNG";
     private static final String QR_FOR_SIGNUP = "https://192.168.0.29:5173/";
 
-    public byte[] generateSignUpQR() {
+    public byte[] generateSignUpQR(QRRequestDTO.generateSignUpQRDTO requestDTO) {
+
+        ByteArrayOutputStream outputStream = getByteArrayOutputStream(QR_FOR_SIGNUP);
+
+        // 회원 가입 용 사진 정보 저장 준비
+        saveSignUpIdentification(requestDTO);
+
+        return outputStream.toByteArray();
+    }
+
+    private void saveSignUpIdentification(QRRequestDTO.generateSignUpQRDTO requestDTO) {
+        Identification identification = Identification.builder()
+                .email(requestDTO.email())
+                .imgUrl(null)
+                .build();
+
+        identificationRedisRepository.save(identification);
+    }
+
+    private static ByteArrayOutputStream getByteArrayOutputStream(String targetUrl) {
 
         try {
             // QR Code - BitMatrix: qr 정보 생성
             BitMatrix bitMatrix = new MultiFormatWriter()
-                    .encode(QR_FOR_SIGNUP, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT);
+                    .encode(targetUrl, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT);
 
             // QR Code - Image 생성
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+            MatrixToImageWriter.writeToStream(bitMatrix, QR_FORMAT, outputStream);
 
-            return outputStream.toByteArray();
+            return outputStream;
 
         } catch (WriterException | IOException e) {
             throw new RuntimeException(e);
