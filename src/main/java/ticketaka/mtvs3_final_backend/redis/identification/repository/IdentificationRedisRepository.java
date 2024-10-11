c9c18e63587a7b1490ebbaf8f4e827f0d2160727
INUK-ai <arheon2469@gmail.com>
2024-10-11 16:41:42 +0900

    Feat: IdentificationRedisRepository - findByEmail

--- src/main/java/ticketaka/mtvs3_final_backend/redis/identification/repository/IdentificationRedisRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/redis/identification/repository/IdentificationRedisRepository.java
@@ -5,4 +5,5 @@ import ticketaka.mtvs3_final_backend.redis.identification.domain.Identification;
 
 public interface IdentificationRedisRepository extends CrudRepository<Identification, Long> {
 
+    Identification findByEmail(String email);
 }
