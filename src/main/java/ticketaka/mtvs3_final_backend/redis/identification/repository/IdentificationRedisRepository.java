facccaeca2214db39cbb64d07fabe117b254042c
INUK-ai <arheon2469@gmail.com>
2024-10-11 16:41:24 +0900

    Feat: Redis - IdentificationRedisRepository

---
+++ src/main/java/ticketaka/mtvs3_final_backend/redis/identification/repository/IdentificationRedisRepository.java
@@ -0,0 +1,8 @@
+package ticketaka.mtvs3_final_backend.redis.identification.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ticketaka.mtvs3_final_backend.redis.identification.domain.Identification;
+
+public interface IdentificationRedisRepository extends CrudRepository<Identification, Long> {
+
+}
