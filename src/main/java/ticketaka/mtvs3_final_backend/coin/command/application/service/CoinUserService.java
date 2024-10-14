9d8e488e9540a897657e15854b1ea57ee8f38400
INUK-ai <arheon2469@gmail.com>
2024-10-14 17:54:04 +0900

    Feat: CoinHistoryRepository

--- src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/service/CoinUserService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/service/CoinUserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ticketaka.mtvs3_final_backend.coin.command.domain.model.CoinAcquisition;
 import ticketaka.mtvs3_final_backend.coin.command.domain.model.CoinHistory;
 import ticketaka.mtvs3_final_backend.coin.command.domain.repository.CoinAcquisitionRepository;
+import ticketaka.mtvs3_final_backend.coin.command.domain.repository.CoinHistoryRepository;
 import ticketaka.mtvs3_final_backend.member.command.domain.repository.MemberRepository;
 
 @Slf4j
