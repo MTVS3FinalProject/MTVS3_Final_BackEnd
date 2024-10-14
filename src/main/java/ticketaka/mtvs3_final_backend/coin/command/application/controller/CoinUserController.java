e100d83a8095f265ec29f1f42ccd1679b7ca9e12
INUK-ai <arheon2469@gmail.com>
2024-10-14 18:08:24 +0900

    Refactor: CoinUserController - purchase

--- src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/controller/CoinUserController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/controller/CoinUserController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
+import ticketaka.mtvs3_final_backend.coin.command.application.dto.CoinUserRequestDTO;
 import ticketaka.mtvs3_final_backend.coin.command.application.service.CoinUserService;
 
 @Slf4j
@@ -21,9 +23,9 @@ public class CoinUserController {
         Coin 구매
      */
     @PostMapping("/purchase")
-    public ResponseEntity<?> purchase() {
+    public ResponseEntity<?> purchase(@RequestBody CoinUserRequestDTO.purchaseDTO requestDTO) {
 
-        coinUserService.purchase();
+        coinUserService.purchase(requestDTO);
 
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
