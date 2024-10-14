38d7ef2fc43bae652f64a84d5a6c7ce743a0c06f
INUK-ai <arheon2469@gmail.com>
2024-10-14 17:47:26 +0900

    Feat: ConUserController - purchase

--- src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/controller/CoinUserController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/controller/CoinUserController.java
@@ -2,9 +2,11 @@ package ticketaka.mtvs3_final_backend.coin.command.application.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,4 +16,14 @@ public class CoinUserController {
 
     private final CoinUserService coinUserService;
 
+    /*
+        Coin 구매
+     */
+    @PostMapping("/purchase")
+    public ResponseEntity<?> purchase() {
+
+        coinUserService.purchase();
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
 }
