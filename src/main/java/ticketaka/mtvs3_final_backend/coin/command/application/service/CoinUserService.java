6632fb0bfb0efd988da3575641b342b82e495e6b
INUK-ai <arheon2469@gmail.com>
2024-10-14 18:12:14 +0900

    Feat: CoinAcquisitionRepository - findByName

--- src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/service/CoinUserService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/service/CoinUserService.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend._core.error.exception.Exception400;
+import ticketaka.mtvs3_final_backend.coin.command.application.dto.CoinUserRequestDTO;
 import ticketaka.mtvs3_final_backend.coin.command.domain.model.CoinAcquisition;
-import ticketaka.mtvs3_final_backend.coin.command.domain.model.CoinHistory;
 import ticketaka.mtvs3_final_backend.coin.command.domain.repository.CoinAcquisitionRepository;
 import ticketaka.mtvs3_final_backend.coin.command.domain.repository.CoinHistoryRepository;
 import ticketaka.mtvs3_final_backend.member.command.domain.repository.MemberRepository;
@@ -24,7 +25,9 @@ public class CoinUserService {
         Coin 구매
      */
     @Transactional
-    public void purchase() {
+    public void purchase(CoinUserRequestDTO.purchaseDTO requestDTO) {
 
+        CoinAcquisition coinAcquisition = coinAcquisitionRepository.findByName(requestDTO.purchaseName())
+                .orElseThrow(() -> new Exception400("잘못된 구매 접근입니다."));
     }
 }
--- src/main/java/ticketaka/mtvs3_final_backend/coin/command/domain/repository/CoinAcquisitionRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/domain/repository/CoinAcquisitionRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ticketaka.mtvs3_final_backend.coin.command.domain.model.CoinAcquisition;
 
+import java.util.Optional;
+
 @Repository
 public interface CoinAcquisitionRepository extends JpaRepository<CoinAcquisition, Long> {
+
+    Optional<Object> findByName(String coinAcquisitionName);
 }
