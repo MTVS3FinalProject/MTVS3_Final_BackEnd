91fa097d81c83ade5721b7dcd64f2e05886e000d
INUK-ai <arheon2469@gmail.com>
2024-10-14 18:06:26 +0900

    Feat: CoinUserRequestDTO - purchaseDTO

---
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/dto/CoinUserRequestDTO.java
@@ -0,0 +1,9 @@
+package ticketaka.mtvs3_final_backend.coin.command.application.dto;
+
+public class CoinUserRequestDTO {
+
+    public record purchaseDTO(
+            String purchaseName
+    ) {
+    }
+}
