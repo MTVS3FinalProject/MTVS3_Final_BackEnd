083222fc368dc0c79b1723e2cb0174e10c748597
INUK-ai <arheon2469@gmail.com>
2024-10-16 08:36:29 +0900

    Refactor: UploadStatus - FAIL 추가

--- src/main/java/ticketaka/mtvs3_final_backend/redis/identification/domain/UploadStatus.java
+++ src/main/java/ticketaka/mtvs3_final_backend/redis/identification/domain/UploadStatus.java
@@ -1,6 +1,6 @@
 package ticketaka.mtvs3_final_backend.redis.identification.domain;
 
 public enum UploadStatus {
-    // 인증 대기, 인증 완료
-    PENDING, COMPLETED
+    // 인증 대기, 인증 완료, 인증 실패
+    PENDING, COMPLETED, FAIL
 }
