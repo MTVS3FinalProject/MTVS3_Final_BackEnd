38662e69bbf7d3d23e6368f1d394ff819ef57a52
INUK-ai <arheon2469@gmail.com>
2024-10-18 20:55:50 +0900

    Feat: ConcertRequestDTO - entranceConcertDTO

--- src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/dto/ConcertRequestDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/dto/ConcertRequestDTO.java
@@ -1,4 +1,7 @@
 package ticketaka.mtvs3_final_backend.concert.command.application.dto;
 
+import java.util.List;
+
 public class ConcertRequestDTO {
+
 }
--- src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/dto/ConcertResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/dto/ConcertResponseDTO.java
@@ -1,4 +1,22 @@
 package ticketaka.mtvs3_final_backend.concert.command.application.dto;
 
+import java.util.List;
+
 public class ConcertResponseDTO {
+
+    public record entranceConcertDTO(
+            String concertName,
+            int year,
+            int month,
+            int day,
+            String time,
+            List<SeatIdDTO> availableSeats,
+            List<SeatIdDTO> receptionSeats,
+            int remainingTickets
+    ) {
+        protected record SeatIdDTO(
+                String seatId
+        ) {
+        }
+    }
 }
