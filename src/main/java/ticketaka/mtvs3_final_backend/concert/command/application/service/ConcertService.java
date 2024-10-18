c7ac70191b46c5d9cf6be2b8eb3b8e9b513c1328
INUK-ai <arheon2469@gmail.com>
2024-10-18 20:01:48 +0900

    Feat: SeatController

--- src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/service/ConcertService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/service/ConcertService.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.concert.command.domain.repository.ConcertRepository;
 
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ConcertService {
+
+    private final ConcertRepository concertRepository;
 }
---
+++ src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/SeatController.java
@@ -0,0 +1,15 @@
+package ticketaka.mtvs3_final_backend.seat.command.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/concert/seat")
+public class SeatController {
+
+
+}
