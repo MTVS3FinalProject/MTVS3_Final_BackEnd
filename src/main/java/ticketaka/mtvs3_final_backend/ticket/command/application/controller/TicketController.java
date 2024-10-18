e0992a2013f5ac2ef118df4d9462d0d2c219e768
INUK-ai <arheon2469@gmail.com>
2024-10-18 20:15:37 +0900

    Feat: TicketController

---
+++ src/main/java/ticketaka/mtvs3_final_backend/ticket/command/application/controller/TicketController.java
@@ -0,0 +1,15 @@
+package ticketaka.mtvs3_final_backend.ticket.command.application.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/concert/ticket")
+public class TicketController {
+
+
+}
