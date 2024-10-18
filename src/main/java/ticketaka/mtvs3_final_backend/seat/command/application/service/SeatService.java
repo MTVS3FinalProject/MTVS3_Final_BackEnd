26b38ac9190283e2f66128ea7dd6e321a5e23638
INUK-ai <arheon2469@gmail.com>
2024-10-18 20:14:21 +0900

    Feat: Ticket domain

--- src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/service/SeatService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/service/SeatService.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.seat.command.domain.repository.SeatRepository;
 
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class SeatService {
+
+    private final SeatRepository seatRepository;
 }
