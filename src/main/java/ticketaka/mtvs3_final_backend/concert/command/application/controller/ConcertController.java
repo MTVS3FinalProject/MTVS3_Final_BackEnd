81c39441707b5440d593443a8710bc0f8a90a2ae
INUK-ai <arheon2469@gmail.com>
2024-10-18 19:52:53 +0900

    Feat: ConcertService

--- src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/controller/ConcertController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/controller/ConcertController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ticketaka.mtvs3_final_backend.concert.command.application.service.ConcertService;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/concert")
 public class ConcertController {
+
+    private final ConcertService concertService;
 }
