b11cac750a092df415336e3c92416f865d28517a
INUK-ai <arheon2469@gmail.com>
2024-10-19 18:11:00 +0900

    Refactor: SeatRequestDTO - seatIdDTO concertName 추가

--- src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/dto/SeatRequestDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/dto/SeatRequestDTO.java
@@ -3,6 +3,7 @@ package ticketaka.mtvs3_final_backend.seat.command.application.dto;
 public class SeatRequestDTO {
 
     public record seatIdDTO(
+            String concertName,
             String seatId
     ) {
     }
