218d2d30f9046876546073dfb189a7bff8e0069a
INUK-ai <arheon2469@gmail.com>
2024-10-19 16:28:10 +0900

    Feat: SeatResponseDTO - getSeatDTO

--- src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/dto/SeatResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/dto/SeatResponseDTO.java
@@ -1,4 +1,12 @@
 package ticketaka.mtvs3_final_backend.seat.command.application.dto;
 
 public class SeatResponseDTO {
+
+    public record getSeatDTO(
+            String seatId,
+            String seatInfo,
+            String drawingTime,
+            int competitionRate
+    ) {
+    }
 }
