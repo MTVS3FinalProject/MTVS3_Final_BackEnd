1ad339396f220be5e82c610922f02bff9661eeff
INUK-ai <arheon2469@gmail.com>
2024-10-19 17:48:18 +0900

    Feat: ConcertController - entranceConcertDTO

--- src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/controller/ConcertController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/controller/ConcertController.java
@@ -2,8 +2,14 @@ package ticketaka.mtvs3_final_backend.concert.command.application.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
+import ticketaka.mtvs3_final_backend.concert.command.application.dto.ConcertRequestDTO;
+import ticketaka.mtvs3_final_backend.concert.command.application.dto.ConcertResponseDTO;
 import ticketaka.mtvs3_final_backend.concert.command.application.service.ConcertService;
 
 @Slf4j
@@ -13,4 +19,15 @@ import ticketaka.mtvs3_final_backend.concert.command.application.service.Concert
 public class ConcertController {
 
     private final ConcertService concertService;
+
+    /*
+        공연장 입장
+     */
+    @PostMapping
+    public ResponseEntity<?> entranceConcertDTO(@RequestBody ConcertRequestDTO.entranceConcertDTO requestDTO) {
+        
+        ConcertResponseDTO.entranceConcertDTO responseDTO = null;
+        
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+    }
 }
