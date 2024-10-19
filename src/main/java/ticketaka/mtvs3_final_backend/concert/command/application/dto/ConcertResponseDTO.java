cc8904fd9b58d930029c5582ea6c3a60f49ed5dd
INUK-ai <arheon2469@gmail.com>
2024-10-19 23:00:52 +0900

    Feat: ConcertResponseDTO - ConcertDateTimeDTO

--- src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/dto/ConcertResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/concert/command/application/dto/ConcertResponseDTO.java
@@ -1,5 +1,8 @@
 package ticketaka.mtvs3_final_backend.concert.command.application.dto;
 
+import ticketaka.mtvs3_final_backend.concert.command.domain.model.Concert;
+
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class ConcertResponseDTO {
@@ -14,9 +17,26 @@ public class ConcertResponseDTO {
             List<SeatIdDTO> receptionSeats,
             int remainingTickets
     ) {
-        protected record SeatIdDTO(
-                String seatId
-        ) {
+    }
+
+    public record SeatIdDTO(
+            String seatId
+    ) {
+    }
+
+    public record ConcertDateTimeDTO(
+            String year,
+            String month,
+            String day,
+            String time
+    ) {
+        public ConcertDateTimeDTO(Concert concert) {
+            this(
+                    concert.getConcertDate().format(DateTimeFormatter.ofPattern("yyyy")),  // yyyy 형식의 연도
+                    concert.getConcertDate().format(DateTimeFormatter.ofPattern("MM")),  // MM 형식의 월
+                    concert.getConcertDate().format(DateTimeFormatter.ofPattern("dd")),  // dd 형식의 일
+                    concert.getConcertDate().toLocalTime().toString()  // 시간 부분
+            );
         }
     }
 }
