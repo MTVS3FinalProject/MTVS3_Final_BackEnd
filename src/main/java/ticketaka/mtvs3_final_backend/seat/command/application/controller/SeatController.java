b75c5cfcc862b288b4da5e8cd83a80bcebb6b379
INUK-ai <arheon2469@gmail.com>
2024-10-19 18:02:18 +0900

    Feat: SeatController - getReceptionSeats

--- src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/controller/SeatController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/seat/command/application/controller/SeatController.java
@@ -3,10 +3,7 @@ package ticketaka.mtvs3_final_backend.seat.command.application.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
 import ticketaka.mtvs3_final_backend.seat.command.application.dto.SeatRequestDTO;
 import ticketaka.mtvs3_final_backend.seat.command.application.dto.SeatResponseDTO;
@@ -38,6 +35,17 @@ public class SeatController {
     public ResponseEntity<?> seatReception(@RequestBody SeatRequestDTO.seatIdDTO requestDTO) {
 
         SeatResponseDTO.seatReceptionDTO responseDTO = null;
+
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+    }
+
+    /*
+        현재 회원이 접수한 좌석 조회
+     */
+    @GetMapping
+    public ResponseEntity<?> getReceptionSeats() {
+
+        SeatResponseDTO.getReceptionSeatsDTO responseDTO = null;
 
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
