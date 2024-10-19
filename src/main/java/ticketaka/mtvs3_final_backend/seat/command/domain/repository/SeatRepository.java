61f1ce544be080aa6d2fc25a3f963e0c443fcff1
INUK-ai <arheon2469@gmail.com>
2024-10-19 22:33:19 +0900

    Feat: SeatRepository - findByConcertAndSeatStatus

--- src/main/java/ticketaka/mtvs3_final_backend/seat/command/domain/repository/SeatRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/seat/command/domain/repository/SeatRepository.java
@@ -2,8 +2,14 @@ package ticketaka.mtvs3_final_backend.seat.command.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ticketaka.mtvs3_final_backend.concert.command.domain.model.Concert;
 import ticketaka.mtvs3_final_backend.seat.command.domain.model.Seat;
+import ticketaka.mtvs3_final_backend.seat.command.domain.model.SeatStatus;
+
+import java.util.List;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findByConcertAndSeatStatus(Concert concert, SeatStatus seatStatus);
 }
