314d5b38583fd851a9cce5c30326e0b103f21b4b
INUK-ai <arheon2469@gmail.com>
2024-10-19 22:08:54 +0900

    Feat: ConcertRepository - findByName

--- src/main/java/ticketaka/mtvs3_final_backend/concert/command/domain/repository/ConcertRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/concert/command/domain/repository/ConcertRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ticketaka.mtvs3_final_backend.concert.command.domain.model.Concert;
 
+import java.util.Optional;
+
 @Repository
 public interface ConcertRepository extends JpaRepository<Concert, Long> {
+
+    Optional<Concert> findByName(String concertName);
 }
