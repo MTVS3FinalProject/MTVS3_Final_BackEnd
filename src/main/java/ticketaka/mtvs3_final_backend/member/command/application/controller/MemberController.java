e6ae4bfb598779a73f14e1e74c460c78169eeaca
INUK-ai <arheon2469@gmail.com>
2024-10-21 20:00:48 +0900

    Feat: MemberController

---
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/application/controller/MemberController.java
@@ -0,0 +1,13 @@
+package ticketaka.mtvs3_final_backend.member.command.application.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class MemberController {
+}
