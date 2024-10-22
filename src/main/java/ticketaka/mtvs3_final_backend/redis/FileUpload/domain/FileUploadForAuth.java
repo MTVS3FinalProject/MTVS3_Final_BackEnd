27583cdc909eee078e010c12a7d8616a5249b3d9
INUK-ai <arheon2469@gmail.com>
2024-10-22 18:23:17 +0900

    Refactor: Camera 통신

--- src/main/java/ticketaka/mtvs3_final_backend/redis/FileUpload/domain/FileUploadForAuth.java
+++ src/main/java/ticketaka/mtvs3_final_backend/redis/FileUpload/domain/FileUploadForAuth.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.core.RedisHash;
 @Getter
 @NoArgsConstructor
 @SuperBuilder
-@RedisHash(value = "fileupload_for_signup", timeToLive = 900) // 60 * 15
+@RedisHash(value = "fileupload_for_auth", timeToLive = 900) // 60 * 15
 public class FileUploadForAuth extends FileUpload {
     @Setter
     private String code;
