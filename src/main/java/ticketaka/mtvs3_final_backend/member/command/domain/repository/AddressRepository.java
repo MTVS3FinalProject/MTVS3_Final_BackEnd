052f2c921f393f52ca2b66c804bfe41b6f008479
INUK-ai <arheon2469@gmail.com>
2024-10-23 15:33:15 +0900

    Feat: AddressRepository - findByMemberId

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/repository/AddressRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/domain/repository/AddressRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ticketaka.mtvs3_final_backend.member.command.domain.model.Address;
 
+import java.util.Optional;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    Optional<Address> findByMemberId(Long currentMemberId);
 }
