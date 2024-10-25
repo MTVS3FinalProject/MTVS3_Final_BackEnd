aaac30bfa8fa5eb197b174798386feee3edf74e4
INUK-ai <arheon2469@gmail.com>
2024-10-25 18:05:13 +0900

    Feat: CoinHistoryService - saveCoinHistory

--- src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/service/CoinHistoryService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/application/service/CoinHistoryService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.coin.command.application.dto.CoinHistoryRequestDTO;
+import ticketaka.mtvs3_final_backend.coin.command.domain.model.CoinHistory;
 import ticketaka.mtvs3_final_backend.coin.command.domain.repository.CoinHistoryRepository;
 
 @Slf4j
@@ -13,4 +15,20 @@ import ticketaka.mtvs3_final_backend.coin.command.domain.repository.CoinHistoryR
 public class CoinHistoryService {
 
     private final CoinHistoryRepository coinHistoryRepository;
+
+    /*
+        Coin History 저장
+     */
+    @Transactional
+    public void saveCoinHistory(CoinHistoryRequestDTO.saveCoinHistoryDTO requestDTO) {
+
+        CoinHistory coinHistory = CoinHistory.builder()
+                .memberId(requestDTO.memberId())
+                .acquisitionType(requestDTO.acquisitionType())
+                .coinAcquisitionId(requestDTO.coinAcquisitionId())
+                .coinUsageType(requestDTO.coinUsageType())
+                .build();
+
+        coinHistoryRepository.save(coinHistory);
+    }
 }
