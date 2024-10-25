873d22f2a8ae9a721b6c5ab276cfa0d9745460eb
INUK-ai <arheon2469@gmail.com>
2024-10-25 15:46:04 +0900

    Feat: CoinCharge

---
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/command/domain/model/CoinCharge.java
@@ -0,0 +1,33 @@
+package ticketaka.mtvs3_final_backend.coin.command.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ticketaka.mtvs3_final_backend.BaseTimeEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "coin_charge_tb")
+public class CoinCharge extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private Long amount;
+    @Column
+    private Long price;
+
+    @Builder
+    public CoinCharge(String name, Long amount, Long price) {
+        this.name = name;
+        this.amount = amount;
+        this.price = price;
+    }
+}
