9eda1ad5da68583fa0104d2c00d221f4b8338e73
INUK-ai <arheon2469@gmail.com>
2024-11-01 08:05:26 +0900

    Feat: SeatQueryRepository

---
+++ src/main/java/ticketaka/mtvs3_final_backend/seat/query/repository/SeatQueryRepository.java
@@ -0,0 +1,9 @@
+package ticketaka.mtvs3_final_backend.seat.query.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ticketaka.mtvs3_final_backend.seat.command.domain.model.Seat;
+
+@Repository
+public interface SeatQueryRepository extends JpaRepository<Seat, Long> {
+}
