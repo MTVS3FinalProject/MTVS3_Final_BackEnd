2e491ce7f2485f851a36ffae0407ea042f38b5e9
INUK-ai <arheon2469@gmail.com>
2024-11-01 18:30:15 +0900

    Feat: MemberSeatCommandService - seatReception

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/memberseat/command/application/service/MemberSeatCommandService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/memberseat/command/application/service/MemberSeatCommandService.java
@@ -1,4 +1,37 @@
 package ticketaka.mtvs3_final_backend.ticketing.memberseat.command.application.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.ticketing.memberseat.command.domain.model.MemberSeat;
+import ticketaka.mtvs3_final_backend.ticketing.memberseat.command.domain.model.MemberSeatStatus;
+import ticketaka.mtvs3_final_backend.ticketing.memberseat.command.domain.repository.MemberSeatCommandRepository;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
 public class MemberSeatCommandService {
+
+    private final MemberSeatCommandRepository memberSeatCommandRepository;
+
+    /*
+        좌석 접수
+     */
+    @Transactional
+    public void seatReception(Long memberId, Long concertId, Long seatId) {
+        MemberSeat memberSeat = newMemberSeat(memberId, concertId, seatId);
+        memberSeatCommandRepository.save(memberSeat);
+    }
+
+    // MemberSeat 생성
+    private static MemberSeat newMemberSeat(Long memberId, Long concertId, Long seatId) {
+        return MemberSeat.builder()
+                .memberId(memberId)
+                .concertId(concertId)
+                .seatId(seatId)
+                .memberSeatStatus(MemberSeatStatus.RECEIVED)
+                .build();
+    }
 }
