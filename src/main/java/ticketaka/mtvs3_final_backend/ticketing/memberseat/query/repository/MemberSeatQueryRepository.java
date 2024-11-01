b778226d63c3bbd1dc1d74e89af98dc3ef0d4287
INUK-ai <arheon2469@gmail.com>
2024-11-01 18:09:09 +0900

    Feat: MemberQueryRepository - findByMemberIdAndConcertIdAndSeatIdAndMemberSeatStatus

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/memberseat/query/repository/MemberSeatQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/memberseat/query/repository/MemberSeatQueryRepository.java
@@ -10,7 +10,12 @@ import java.util.Optional;
 @Repository
 public interface MemberSeatQueryRepository extends JpaRepository<MemberSeat, Long> {
 
+    // 해당 좌석에 접수한 회원 수 조회
     int countByConcertIdAndSeatIdAndMemberSeatStatus(Long concertId, Long seatId, MemberSeatStatus status);
 
+    // MemberSeat 조회
     Optional<MemberSeat> findByMemberIdAndConcertIdAndSeatId(Long memberId, Long concertId, Long seatId);
+
+    // 이미 접수된 Seat 인지 검사
+    Optional<MemberSeat> findByMemberIdAndConcertIdAndSeatIdAndMemberSeatStatus(Long currentMemberId, Long concertId, Long seatId, MemberSeatStatus memberSeatStatus);
 }
