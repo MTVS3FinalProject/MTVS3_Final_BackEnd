69988b93ce8d15f60c956894ec2890c83a1d750b
INUK-ai <arheon2469@gmail.com>
2024-11-05 17:19:07 +0900

    Feat: Ticket

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/ticket/command/domain/model/Ticket.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/ticket/command/domain/model/Ticket.java
@@ -2,9 +2,12 @@ package ticketaka.mtvs3_final_backend.ticketing.ticket.command.domain.model;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ticketaka.mtvs3_final_backend.BaseTimeEntity;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -15,4 +18,38 @@ public class Ticket extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+    @Column(nullable = false)
+    private Long concertId;
+    @Column(nullable = false)
+    private Long seatId;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private TicketStatus ticketStatus;
+
+    @Column(nullable = false, unique = true)
+    private String ticketNumber;
+    @Column(nullable = false)
+    private Integer ticketPrice;
+
+    @Column
+    private LocalDateTime issuedTime;
+
+    @Column
+    private String qrCode;
+
+    @Builder
+    public Ticket(Long memberId, Long concertId, Long seatId, TicketStatus ticketStatus, String ticketNumber, Integer ticketPrice) {
+        this.memberId = memberId;
+        this.concertId = concertId;
+        this.seatId = seatId;
+        this.ticketStatus = ticketStatus;
+        this.ticketNumber = ticketNumber;
+        this.ticketPrice = ticketPrice;
+        this.issuedTime = null;
+        this.qrCode = null;
+    }
 }
