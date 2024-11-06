50158ddc8390502a82e5fcef9c2fac62c13356bf
INUK-ai <arheon2469@gmail.com>
2024-11-06 13:57:07 +0900

    Feat: Inventory 조회 용 Service setup

--- src/main/java/ticketaka/mtvs3_final_backend/member/query/service/MemberInventoryService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/query/service/MemberInventoryService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.sticker.query.service.StickerQueryService;
+import ticketaka.mtvs3_final_backend.ticketing.ticket.query.service.TicketQueryService;
 import ticketaka.mtvs3_final_backend.title.query.service.TitleQueryService;
 
 @Slf4j
