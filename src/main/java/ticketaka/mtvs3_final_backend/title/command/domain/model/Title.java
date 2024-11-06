9868d852db7563d2a78293ffbca39edd3cfcdd76
INUK-ai <arheon2469@gmail.com>
2024-11-06 13:15:58 +0900

    Feat: Title

---
+++ src/main/java/ticketaka/mtvs3_final_backend/title/command/domain/model/Title.java
@@ -0,0 +1,35 @@
+package ticketaka.mtvs3_final_backend.title.command.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ticketaka.mtvs3_final_backend.BaseTimeEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "title_tb")
+public class Title extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String titleName;
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Rarity rarity;
+
+    @Builder
+    public Title(String titleName, String description, Rarity rarity) {
+        this.titleName = titleName;
+        this.description = description;
+        this.rarity = rarity;
+    }
+}
