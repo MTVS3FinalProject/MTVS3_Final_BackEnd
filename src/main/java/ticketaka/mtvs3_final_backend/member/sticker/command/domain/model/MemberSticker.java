e4250651e7fee4ba797c5118b6d38d2c4c5c6503
INUK-ai <arheon2469@gmail.com>
2024-11-06 14:35:45 +0900

    Feat: MemberSticker

---
+++ src/main/java/ticketaka/mtvs3_final_backend/member/sticker/command/domain/model/MemberSticker.java
@@ -0,0 +1,30 @@
+package ticketaka.mtvs3_final_backend.member.sticker.command.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ticketaka.mtvs3_final_backend.BaseTimeEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member_sticker_tb")
+public class MemberSticker extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long memberId;
+    @Column
+    private Long stickerId;
+
+    @Builder
+    public MemberSticker(Long memberId, Long stickerId) {
+        this.memberId = memberId;
+        this.stickerId = stickerId;
+    }
+}
--- src/main/java/ticketaka/mtvs3_final_backend/member/title/command/domain/model/MemberTitle.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/title/command/domain/model/MemberTitle.java
@@ -1,4 +1,4 @@
-package ticketaka.mtvs3_final_backend.title.membertitle.command.domain.model;
+package ticketaka.mtvs3_final_backend.member.title.command.domain.model;
 
 import jakarta.persistence.*;
 import lombok.*;
