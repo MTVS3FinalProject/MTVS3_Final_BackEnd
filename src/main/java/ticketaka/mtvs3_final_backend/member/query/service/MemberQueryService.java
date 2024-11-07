8e4b7fc233f207392917d39a17bdcc315e2ad68a
INUK-ai <arheon2469@gmail.com>
2024-11-07 23:16:59 +0900

    Feat: getMemberInventory - memberTitleDTOList

--- src/main/java/ticketaka/mtvs3_final_backend/member/query/service/MemberQueryService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/query/service/MemberQueryService.java
@@ -4,7 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend._core.error.exception.Exception401;
+import ticketaka.mtvs3_final_backend.member.command.domain.model.Member;
+import ticketaka.mtvs3_final_backend.member.query.dto.MemberQueryResponseDTO;
 import ticketaka.mtvs3_final_backend.member.query.repository.MemberQueryRepository;
+import ticketaka.mtvs3_final_backend.member.title.command.domain.model.MemberTitle;
+import ticketaka.mtvs3_final_backend.member.title.command.domain.repository.MemberTitleQueryRepository;
+import ticketaka.mtvs3_final_backend.title.command.domain.model.Title;
+import ticketaka.mtvs3_final_backend.title.query.service.TitleQueryService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -12,7 +23,42 @@ import ticketaka.mtvs3_final_backend.member.query.repository.MemberQueryReposito
 @Service
 public class MemberQueryService {
 
-    private final MemberInventoryService memberInventoryService;
-
     private final MemberQueryRepository memberQueryRepository;
+    private final MemberTitleQueryRepository memberTitleQueryRepository;
+    private final TitleQueryService titleQueryService;
+
+    public MemberQueryResponseDTO.getMemberInventoryDTO getMemberInventory(Long memberId) {
+
+        // Member 조회
+        getMember(memberId);
+        
+        // Title List 조회
+        List<MemberTitle> memberTitleList = memberTitleQueryRepository.findAllByMemberId(memberId);
+        Map<Long, Title> titleMap = titleQueryService.getMemberTitleList(memberTitleList);
+        List<MemberQueryResponseDTO.getMemberTitleDTO> memberTitleDTOList = memberTitleList.stream()
+                .map(memberTitle -> {
+                    Title title = titleMap.get(memberTitle.getTitleId());
+
+                    return new MemberQueryResponseDTO.getMemberTitleDTO(
+                            title.getId().intValue(),
+                            title.getTitleName(),
+                            title.getTitleScript(),
+                            title.getTitleRarity().toString(),
+                            memberTitle.getIsRepresentative()
+                    );
+                })
+                .toList();
+
+        // Sticker List 조회
+        
+        // Custom Ticket List 조회
+
+        return null;
+    }
+
+    // Member 조회
+    private Member getMember(Long memberId) {
+        return memberQueryRepository.findById(memberId)
+                .orElseThrow(() -> new Exception401("해당 회원을 찾을 수 없습니다."));
+    }
 }
