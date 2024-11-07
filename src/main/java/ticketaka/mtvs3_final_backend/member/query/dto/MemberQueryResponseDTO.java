e5d413be9dc9bcb89e2920a5c8d77655a15a43a7
INUK-ai <arheon2469@gmail.com>
2024-11-07 22:53:00 +0900

    Feat: MemberQueryResponseDTO - getMemberTitleDTO

--- src/main/java/ticketaka/mtvs3_final_backend/member/query/dto/MemberQueryResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/query/dto/MemberQueryResponseDTO.java
@@ -1,11 +1,23 @@
 package ticketaka.mtvs3_final_backend.member.query.dto;
 
+import java.util.List;
+
 public class MemberQueryResponseDTO {
 
     public record getMemberInventoryDTO(
             // Title List
+            List<getMemberTitleDTO> memberTitleDTOList
             // Sticker List
             // CustomTicket List
+    ) {
+    }
+
+    public record getMemberTitleDTO(
+            int titleId,
+            String titleName,
+            String titleScript,
+            String titleRarity,
+            Boolean isRepresentative
     ) {
     }
 }
