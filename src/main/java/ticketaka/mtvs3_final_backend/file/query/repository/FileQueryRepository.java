870e563cb7415f1203a69e759eb88c77e09958ae
INUK-ai <arheon2469@gmail.com>
2024-11-07 11:16:59 +0900

    Feat: FileQueryRepository - findAllByRelationTypeAndRelationIdIn

--- src/main/java/ticketaka/mtvs3_final_backend/file/query/repository/FileQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/file/query/repository/FileQueryRepository.java
@@ -3,7 +3,12 @@ package ticketaka.mtvs3_final_backend.file.query.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ticketaka.mtvs3_final_backend.file.command.domain.model.File;
+import ticketaka.mtvs3_final_backend.file.command.domain.model.property.RelationType;
+
+import java.util.List;
 
 @Repository
 public interface FileQueryRepository extends JpaRepository<File, Long> {
+
+    List<File> findAllByRelationTypeAndRelationIdIn(RelationType relationType, List<Long> stickerIdList);
 }
