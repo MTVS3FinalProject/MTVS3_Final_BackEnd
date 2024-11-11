84b5babd8fafe27d856072e1c6a0bde3d9250082
INUK-ai <arheon2469@gmail.com>
2024-11-11 13:16:17 +0900

    Feat: StickerRarity - fromString

--- src/main/java/ticketaka/mtvs3_final_backend/sticker/command/domain/model/StickerRarity.java
+++ src/main/java/ticketaka/mtvs3_final_backend/sticker/command/domain/model/StickerRarity.java
@@ -1,5 +1,18 @@
 package ticketaka.mtvs3_final_backend.sticker.command.domain.model;
 
 public enum StickerRarity {
-    COMMON, RARE, UNIQUE
+    COMMON, RARE, UNIQUE;
+
+    public static StickerRarity fromString(String stickerRarity) {
+
+        if (stickerRarity == null) {
+            return COMMON;
+        }
+
+        return switch (stickerRarity) {
+            case "rare" -> RARE;
+            case "unique" -> UNIQUE;
+            default -> COMMON;
+        };
+    }
 }
