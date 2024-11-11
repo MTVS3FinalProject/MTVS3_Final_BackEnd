35576dd724d3ecdd8fd60eeebf7e17a8b4ee2b54
INUK-ai <arheon2469@gmail.com>
2024-11-11 15:01:13 +0900

    Feat: ConcertCommandService - acquireStickerFromPuzzleResult

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/concert/command/application/service/ConcertCommandService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/concert/command/application/service/ConcertCommandService.java
@@ -30,6 +30,11 @@ import ticketaka.mtvs3_final_backend.ticketing.seat.command.domain.model.Seat;
 import ticketaka.mtvs3_final_backend.ticketing.seat.command.domain.model.SeatStatus;
 import ticketaka.mtvs3_final_backend.ticketing.seat.command.domain.repository.SeatCommandRepository;
 import ticketaka.mtvs3_final_backend.ticketing.seat.query.repository.SeatQueryRepository;
+import ticketaka.mtvs3_final_backend.title.command.domain.model.Title;
+import ticketaka.mtvs3_final_backend.title.command.domain.model.TitleRarity;
+import ticketaka.mtvs3_final_backend.title.member.command.domain.model.MemberTitle;
+import ticketaka.mtvs3_final_backend.title.member.command.domain.repository.MemberTitleCommandRepository;
+import ticketaka.mtvs3_final_backend.title.query.service.TitleQueryService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -52,6 +57,8 @@ public class ConcertCommandService {
 
     private final DrawResultRedisRepository drawResultRedisRepository;
     private final TicketAddressRedisRepository ticketAddressRedisRepository;
+    private final TitleQueryService titleQueryService;
+    private final MemberTitleCommandRepository memberTitleCommandRepository;
 
     /*
         공연장 정보 조회
@@ -104,13 +111,18 @@ public class ConcertCommandService {
     }
 
     /*
-        Puzzle 결과 Sticker 획득
+        Puzzle 결과 Title, Sticker 획득
      */
     public ConcertCommandResponseDTO.acquireStickerFromPuzzleResultDTO acquireStickerFromPuzzleResult(Long memberId, Long concertId, ConcertCommandRequestDTO.acquireStickerFromPuzzleResultDTO requestDTO) {
 
+        // Title 할당
+        Title title = titleQueryService.getPuzzleResult(memberId, concertId, TitleRarity.fromInt(requestDTO.rank()));
         // Sticker 할당
         Sticker sticker = stickerQueryService.getPuzzleResult(memberId, concertId, StickerRarity.fromInt(requestDTO.rank()));
 
+        // Member Title 할당
+        MemberTitle memberTitle = newMemberTitle(memberId, title.getId());
+        memberTitleCommandRepository.save(memberTitle);
         // Member Sticker 생성
         MemberSticker memberSticker = newMemberSticker(memberId, sticker.getId());
         memberStickerCommandRepository.save(memberSticker);
@@ -119,11 +131,19 @@ public class ConcertCommandService {
         String stickerImage = fileQueryService.getFileImage(RelationType.STICKER, sticker.getId());
 
         return new ConcertCommandResponseDTO.acquireStickerFromPuzzleResultDTO(
-                sticker.getId().intValue(),
-                sticker.getStickerName(),
-                sticker.getStickerScript(),
-                sticker.getStickerRarity().toString(),
-                stickerImage
+                new ConcertCommandResponseDTO.titleInfoDTO(
+                        title.getId().intValue(),
+                        title.getTitleName(),
+                        title.getTitleScript(),
+                        title.getTitleRarity().toString()
+                ),
+                new ConcertCommandResponseDTO.stickerInfoDTO(
+                        sticker.getId().intValue(),
+                        sticker.getStickerName(),
+                        sticker.getStickerScript(),
+                        sticker.getStickerRarity().toString(),
+                        stickerImage
+                )
         );
     }
 
@@ -180,6 +200,14 @@ public class ConcertCommandService {
                 .userPhoneNumber(requestDTO.userPhoneNumber())
                 .userAddress1(requestDTO.userAddress1())
                 .userAddress2(requestDTO.userAddress2())
+                .build();
+    }
+
+    // MemberTitle 생성
+    private MemberTitle newMemberTitle(Long memberId, Long titleId) {
+        return MemberTitle.builder()
+                .memberId(memberId)
+                .titleId(titleId)
                 .build();
     }
 
