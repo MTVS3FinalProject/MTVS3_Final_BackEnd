8af4d8d3d047ba67322dc7e143713c1e2f018540
INUK-ai <arheon2469@gmail.com>
2024-11-11 13:14:30 +0900

    Feat: StickerType - fromString

--- src/main/java/ticketaka/mtvs3_final_backend/sticker/command/domain/model/StickerType.java
+++ src/main/java/ticketaka/mtvs3_final_backend/sticker/command/domain/model/StickerType.java
@@ -1,5 +1,17 @@
 package ticketaka.mtvs3_final_backend.sticker.command.domain.model;
 
 public enum StickerType {
-    COMMON, COLLECTION
+    COMMON, COLLECTION, UNKNOWN;
+
+    public static StickerType fromString(String stickerType) {
+        if (stickerType == null) {
+            return UNKNOWN;
+        }
+
+        return switch (stickerType) {
+            case "common" -> COMMON;
+            case "collection" -> COLLECTION;
+            default -> UNKNOWN;
+        };
+    }
 }
