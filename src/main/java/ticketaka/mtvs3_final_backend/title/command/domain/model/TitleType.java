98f44cbdf1fabe20d06acefaf1a8af51f7fe3c10
INUK-ai <arheon2469@gmail.com>
2024-11-11 16:11:45 +0900

    Feat: TitleType - fromString

--- src/main/java/ticketaka/mtvs3_final_backend/title/command/domain/model/TitleType.java
+++ src/main/java/ticketaka/mtvs3_final_backend/title/command/domain/model/TitleType.java
@@ -1,5 +1,18 @@
 package ticketaka.mtvs3_final_backend.title.command.domain.model;
 
 public enum TitleType {
-    GENERAL, CONCERT
+    GENERAL, CONCERT, UNKNOWN;
+
+    public static TitleType fromString(String titleType) {
+
+        if (titleType == null) {
+            return UNKNOWN;
+        }
+
+        return switch (titleType.toLowerCase()) {
+            case "general" -> GENERAL;
+            case "concert" -> CONCERT;
+            default -> UNKNOWN;
+        };
+    }
 }
