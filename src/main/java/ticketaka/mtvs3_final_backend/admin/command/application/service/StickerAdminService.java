a1fd3d05d1d2c1769bc4d64a3928bdbe134f784c
INUK-ai <arheon2469@gmail.com>
2024-11-11 13:22:25 +0900

    Feat: StickerAdminService - saveSticker

---
+++ src/main/java/ticketaka/mtvs3_final_backend/admin/command/application/service/StickerAdminService.java
@@ -0,0 +1,38 @@
+package ticketaka.mtvs3_final_backend.admin.command.application.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.admin.command.application.dto.AdminCommandRequestDTO;
+import ticketaka.mtvs3_final_backend.admin.command.domain.repository.StickerAdminRepository;
+import ticketaka.mtvs3_final_backend.sticker.command.domain.model.Sticker;
+import ticketaka.mtvs3_final_backend.sticker.command.domain.model.StickerRarity;
+import ticketaka.mtvs3_final_backend.sticker.command.domain.model.StickerType;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class StickerAdminService {
+
+    private final StickerAdminRepository stickerAdminRepository;
+
+    /*
+        Sticker 추가
+     */
+    public Sticker saveSticker(Long concertId, AdminCommandRequestDTO.uploadStickerDTO requestDTO) {
+        return stickerAdminRepository.save(newSticker(concertId, requestDTO));
+    }
+
+    // Sticker 생성
+    private static Sticker newSticker(Long concertId, AdminCommandRequestDTO.uploadStickerDTO requestDTO) {
+        return Sticker.builder()
+                .concertId(concertId)
+                .stickerName(requestDTO.stickerName())
+                .stickerScript(requestDTO.stickerScript())
+                .stickerType(StickerType.fromString(requestDTO.stickerType()))
+                .stickerRarity(StickerRarity.fromString(requestDTO.stickerRarity()))
+                .build();
+    }
+}
