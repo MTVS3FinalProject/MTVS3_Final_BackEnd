1b619832ecf97582b2d2cfbb952bf942f52b74e0
INUK-ai <arheon2469@gmail.com>
2024-11-14 01:15:42 +0900

    Feat: KakaoAPIFeignClient - sendKakaoMessage

--- src/main/java/ticketaka/mtvs3_final_backend/admin/command/domain/service/KakaoAPIFeignClient.java
+++ src/main/java/ticketaka/mtvs3_final_backend/admin/command/domain/service/KakaoAPIFeignClient.java
@@ -2,12 +2,19 @@ package ticketaka.mtvs3_final_backend.admin.command.domain.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import ticketaka.mtvs3_final_backend.admin.command.application.dto.KakaoFeignClientResponseDTO;
+import ticketaka.mtvs3_final_backend.admin.command.domain.dto.KakaoFeignClientRequestDTO;
+import ticketaka.mtvs3_final_backend.admin.command.domain.dto.KakaoFeignClientResponseDTO;
 
 @FeignClient(name = "kakao-api-service", url = "https://kapi.kakao.com")
 public interface KakaoAPIFeignClient {
 
     @GetMapping(value = "/v1/api/talk/friends")
     KakaoFeignClientResponseDTO.KakaoFriendListDTO getKakaoFriends(@RequestHeader("Authorization") String accessToken);
+
+    @PostMapping(value = "v1/api/talk/friends/message/send")
+    void sendKakaoMessage(@RequestHeader("Authorization") String accessToken,
+                          @RequestBody KakaoFeignClientRequestDTO.sendKakaoMessageDTO requestDTO);
 }
