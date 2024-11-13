bb1422d917968cc4e4f344e2e84900727c7b13b8
INUK-ai <arheon2469@gmail.com>
2024-11-13 17:40:30 +0900

    Feat: KakaoFeignClient - getKakaoFriends

--- src/main/java/ticketaka/mtvs3_final_backend/admin/command/domain/service/KakaoFeignClient.java
+++ src/main/java/ticketaka/mtvs3_final_backend/admin/command/domain/service/KakaoFeignClient.java
@@ -1,16 +1,21 @@
 package ticketaka.mtvs3_final_backend.admin.command.domain.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import ticketaka.mtvs3_final_backend.admin.command.application.dto.KakaoFeignClientResponseDTO;
 
-@FeignClient(name = "kakao-service", url = "https://kauth.kakao.com/oauth")
+@FeignClient(name = "kakao-service", url = "https://kauth.kakao.com")
 public interface KakaoFeignClient {
 
-    @PostMapping(value = "/token", consumes = "application/x-www-form-urlencoded")
+    @PostMapping(value = "/oauth/token", consumes = "application/x-www-form-urlencoded")
     KakaoFeignClientResponseDTO.KakaoTokenDTO getKakaoToken(@RequestParam("grant_type") String grantType,
                                                             @RequestParam("client_id") String clientId,
                                                             @RequestParam("redirect_uri") String redirectUri,
                                                             @RequestParam("code") String code);
+
+    @GetMapping(value = "/v1/api/talk/friends")
+    KakaoFeignClientResponseDTO.KakaoFriendListDTO getKakaoFriends(@RequestHeader("Authorization") String accessToken);
 }
