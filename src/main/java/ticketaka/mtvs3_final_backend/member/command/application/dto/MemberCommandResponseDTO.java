1a4bee16951974520a89c4e05d4153c76f261857
INUK-ai <arheon2469@gmail.com>
2024-11-15 04:26:50 +0900

    Feat: Title 관련 API

--- src/main/java/ticketaka/mtvs3_final_backend/member/command/application/dto/MemberCommandResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/command/application/dto/MemberCommandResponseDTO.java
@@ -1,4 +1,10 @@
 package ticketaka.mtvs3_final_backend.member.command.application.dto;
 
 public class MemberCommandResponseDTO {
+
+    public record changeMainTitleDTO(
+            String titleName,
+            String titleRarity
+    ) {
+    }
 }
