101ee85c1511debaaba7d477f8bba1d28e9ae56f
INUK-ai <arheon2469@gmail.com>
2024-11-19 21:54:31 +0900

    Feat: MailCommandRepository - findAllByMemberId

--- src/main/java/ticketaka/mtvs3_final_backend/mail/command/domain/repository/MailCommandRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/mail/command/domain/repository/MailCommandRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ticketaka.mtvs3_final_backend.mail.command.domain.model.Mail;
 
+import java.util.List;
+
 @Repository
 public interface MailCommandRepository extends JpaRepository<Mail, Long> {
+
+    List<Mail> findAllByMemberId(Long memberId);
 }
