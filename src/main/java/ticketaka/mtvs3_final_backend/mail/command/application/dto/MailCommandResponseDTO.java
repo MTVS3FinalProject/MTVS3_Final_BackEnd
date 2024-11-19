2ff066746cdbf1591e30a56fe9f66f57854ce67d
INUK-ai <arheon2469@gmail.com>
2024-11-19 21:53:25 +0900

    Refactor: MailCommandResponseDTO - mailDTO

--- src/main/java/ticketaka/mtvs3_final_backend/mail/command/application/dto/MailCommandResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/mail/command/application/dto/MailCommandResponseDTO.java
@@ -1,8 +1,15 @@
 package ticketaka.mtvs3_final_backend.mail.command.application.dto;
 
+import java.util.List;
+
 public class MailCommandResponseDTO {
 
     public record getMailListDTO(
+            List<mailDTO> mailListDTO
+    ) {
+    }
+
+    public record mailDTO(
             int mailId,
             String subject,
             String content,
