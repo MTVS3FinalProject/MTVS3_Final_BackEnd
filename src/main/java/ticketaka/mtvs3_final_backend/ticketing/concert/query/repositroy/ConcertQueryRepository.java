1909926cca959a6e45c99af73ab9e7739d11a1c1
INUK-ai <arheon2469@gmail.com>
2024-11-25 16:20:14 +0900

    Feat: ConcertQueryService - getConcertThumbnailList

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/concert/query/repositroy/ConcertQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/concert/query/repositroy/ConcertQueryRepository.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Repository;
 import ticketaka.mtvs3_final_backend.ticketing.concert.command.domain.model.Concert;
 import ticketaka.mtvs3_final_backend.ticketing.concert.command.domain.model.ConcertStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ConcertQueryRepository extends JpaRepository<Concert, Long> {
 
     Optional<Concert> findByIdAndConcertStatus(Long concertId, ConcertStatus concertStatus);
+
+    List<String> findThumbnailByMemberId(Long memberId);
 }
--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/concert/query/service/ConcertQueryService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/concert/query/service/ConcertQueryService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ticketaka.mtvs3_final_backend.ticketing.concert.command.domain.model.Concert;
+import ticketaka.mtvs3_final_backend.ticketing.concert.query.dto.ConcertQueryResponseDTO;
 import ticketaka.mtvs3_final_backend.ticketing.concert.query.repositroy.ConcertQueryRepository;
 
 import java.util.List;
@@ -20,5 +21,15 @@ public class ConcertQueryService {
     // ConcertList 조회
     public List<Concert> getConcertList(List<Long> concertIdList) {
         return concertQueryRepository.findAllById(concertIdList);
+    }
+
+    /*
+        티켓을 보유한 공연장 썸네일 목록 조회
+     */
+    public ConcertQueryResponseDTO.getConcertThumbnailList getConcertThumbnailList(Long memberId) {
+
+        List<String> concertThumbnails = concertQueryRepository.findThumbnailByMemberId(memberId);
+
+        return new ConcertQueryResponseDTO.getConcertThumbnailList(concertThumbnails);
     }
 }
