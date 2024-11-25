e8cc571ccd81292bfc89702198b8e4b641f849b5
INUK-ai <arheon2469@gmail.com>
2024-11-25 20:52:15 +0900

    Feat: TicketQueryRepository - findTicketDTOByMemberIdAndTicketId

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/ticket/query/repository/TicketQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/ticket/query/repository/TicketQueryRepository.java
@@ -50,4 +50,25 @@ public interface TicketQueryRepository extends JpaRepository<Ticket, Long> {
             "LEFT JOIN File qf ON qf.relationId = t.id AND qf.relationType = 'TICKET' AND qf.filePurpose = 'VERIFICATION' " +
             "WHERE t.memberId = :memberId")
     List<getTicketDTO> findCustomizableTicketsByMemberId(@Param("memberId") Long memberId);
+
+
+    @Query("SELECT new ticketaka.mtvs3_final_backend.ticketing.ticket.query.dto.getTicketDTO(" +
+            "t.id, c.name, c.concertDate, " +
+            "CONCAT(s.section, '구역 ', s.number, '번')," +
+            "CASE WHEN ct.id IS NOT NULL " +
+            "   THEN ctf.fileUrl " +
+            "   ELSE tf.fileUrl " +
+            "END, " +
+            "qf.fileUrl) " +
+            "FROM Ticket t JOIN Concert c ON t.concertId = c.id " +
+            "JOIN Seat s ON t.seatId = s.id " +
+            "LEFT JOIN CustomTicket ct ON t.id = ct.ticketId AND ct.createdAt = (" +
+            "   SELECT MAX(subCt.createdAt) FROM CustomTicket subCt WHERE subCt.ticketId = t.id" +
+            ") " +
+            "LEFT JOIN File ctf ON ctf.relationId = ct.id AND ctf.relationType = 'CUSTOM_TICKET' " +
+            "LEFT JOIN File tf ON tf.relationId = c.id AND tf.relationType = 'CONCERT' AND tf.filePurpose = 'TICKET' " +
+            "LEFT JOIN File qf ON qf.relationId = t.id AND qf.relationType = 'TICKET' AND qf.filePurpose = 'VERIFICATION' " +
+            "WHERE t.memberId = :memberId " +
+            "AND t.id = :ticketId")
+    getTicketDTO findTicketDTOByMemberIdAndTicketId(@Param("memberId") Long memberId, @Param("ticketId") Long ticketId);
 }
