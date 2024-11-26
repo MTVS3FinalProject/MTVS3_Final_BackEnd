9986ca147cd00e79748948907c7ba84fb3938ae8
INUK-ai <arheon2469@gmail.com>
2024-11-26 23:58:00 +0900

    Feat: AdminTicketController - consumeTicket

--- src/main/java/ticketaka/mtvs3_final_backend/admin/command/application/controller/AdminTicketController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/admin/command/application/controller/AdminTicketController.java
@@ -3,10 +3,7 @@ package ticketaka.mtvs3_final_backend.admin.command.application.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
 import ticketaka.mtvs3_final_backend.admin.command.application.dto.AdminVerificationResponseDTO;
 import ticketaka.mtvs3_final_backend.admin.command.application.service.AdminCommandService;
@@ -15,18 +12,29 @@ import ticketaka.mtvs3_final_backend.admin.command.application.service.AdminComm
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/admin")
-public class AdminVerificationController {
+public class AdminTicketController {
 
     private final AdminCommandService adminCommandService;
 
     /*
         Ticket Verification
      */
-    @PostMapping("/verification/ticket/{ticketId}")
+    @GetMapping("/verification/tickets/{ticketId}")
     public ResponseEntity<?> verifyTicket(@PathVariable("ticketId") Long ticketId) {
 
         AdminVerificationResponseDTO.verifyTicketDTO responseDTO = adminCommandService.verifyTicket(ticketId);
 
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+    }
+
+    /*
+        Use Ticket
+     */
+    @PutMapping("/consume/tickets/{ticketId}")
+    public ResponseEntity<?> consumeTicket(@PathVariable("ticketId") Long ticketId) {
+
+        adminCommandService.consumeTicket(ticketId);
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 }
