ce65d7993017ebec4d3aa2be6c538bb38ff24260
INUK-ai <arheon2469@gmail.com>
2024-11-26 23:33:24 +0900

    Feat: AdminVerificationResponseDTO - verifyTicketDTO

--- src/main/java/ticketaka/mtvs3_final_backend/admin/command/application/dto/AdminVerificationResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/admin/command/application/dto/AdminVerificationResponseDTO.java
@@ -3,7 +3,12 @@ package ticketaka.mtvs3_final_backend.admin.command.application.dto;
 public class AdminVerificationResponseDTO {
 
     public record verifyTicketDTO(
-
+            String concertName,
+            int year,
+            int month,
+            int day,
+            String time,
+            String seatInfo
     ) {
     }
 }
