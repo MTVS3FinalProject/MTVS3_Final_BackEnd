3255aa2fd63747fed40776b587c15243024e754f
INUK-ai <arheon2469@gmail.com>
2024-12-03 00:45:08 +0900

    Feat: PuzzleResult 관련 Mail

---
+++ src/main/java/ticketaka/mtvs3_final_backend/mail/command/domain/model/MailPuzzleResult.java
@@ -0,0 +1,31 @@
+package ticketaka.mtvs3_final_backend.mail.command.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ticketaka.mtvs3_final_backend.BaseTimeEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "mail_puzzle_result_tb")
+public class MailPuzzleResult extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long mailId;
+    @Column(nullable = false)
+    private Long puzzleResultId;
+
+    @Builder
+    public MailPuzzleResult(Long id, Long mailId, Long puzzleResultId) {
+        this.id = id;
+        this.mailId = mailId;
+        this.puzzleResultId = puzzleResultId;
+    }
+}
