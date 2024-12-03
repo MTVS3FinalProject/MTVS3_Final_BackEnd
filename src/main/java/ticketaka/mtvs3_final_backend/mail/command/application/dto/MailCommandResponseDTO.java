eff9c1686e351b00a8f47dbe6c5565350913ca30
INUK-ai <arheon2469@gmail.com>
2024-12-03 13:39:31 +0900

    Feat: MailCommandResponseDTO - readPuzzleMailDTO

--- src/main/java/ticketaka/mtvs3_final_backend/mail/command/application/dto/MailCommandResponseDTO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/mail/command/application/dto/MailCommandResponseDTO.java
@@ -1,5 +1,8 @@
 package ticketaka.mtvs3_final_backend.mail.command.application.dto;
 
+import ticketaka.mtvs3_final_backend.member.query.dto.getMemberStickerDTO;
+import ticketaka.mtvs3_final_backend.member.query.dto.getMemberTitleDTO;
+
 public class MailCommandResponseDTO {
 
     public record readMailDTO(
@@ -13,6 +16,17 @@ public class MailCommandResponseDTO {
     public record readPostponeMailDTO(
             int concertId,
             int seatId
+    ) {
+    }
+
+    public record readPuzzleMailDTO(
+            int mailId,
+            String subject,
+            String content,
+            String mailCategory,
+            int rank,
+            getMemberTitleDTO titleInfo,
+            getMemberStickerDTO stickerINfo
     ) {
     }
 }
