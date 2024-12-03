d5c6ebe7f3b5da5cf0b698c1cfc93e417c6fbdf2
INUK-ai <arheon2469@gmail.com>
2024-12-03 13:51:56 +0900

    Feat: PuzzleResultQueryRepository - getPuzzleResultByMailId

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/puzzle/query/repository/PuzzleResultQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/puzzle/query/repository/PuzzleResultQueryRepository.java
@@ -1,10 +1,17 @@
 package ticketaka.mtvs3_final_backend.ticketing.puzzle.query.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ticketaka.mtvs3_final_backend.ticketing.puzzle.command.domain.model.PuzzleResult;
 
 @Repository
 public interface PuzzleResultQueryRepository extends JpaRepository<PuzzleResult, Long> {
 
+    @Query("SELECT pr " +
+            "FROM PuzzleResult pr " +
+            "JOIN MailPuzzleResult mpr ON pr.id = mpr.puzzleResultId " +
+            "WHERE mpr.mailId = :mailId")
+    PuzzleResult getPuzzleResultByMailId(@Param("mailId") Long mailId);
 }
