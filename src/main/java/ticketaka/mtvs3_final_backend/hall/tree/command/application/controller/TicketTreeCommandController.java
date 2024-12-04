6e4e1d01984680e78c39d899a32147cc17a6f50f
INUK-ai <arheon2469@gmail.com>
2024-12-04 14:49:52 +0900

    Feat: TicketTreeCommandService setup

---
+++ src/main/java/ticketaka/mtvs3_final_backend/hall/tree/command/application/controller/TicketTreeCommandController.java
@@ -0,0 +1,17 @@
+package ticketaka.mtvs3_final_backend.hall.tree.command.application.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/hall/tree")
+@Tag(name = "TicketTreeCommandController")
+public class TicketTreeCommandController {
+
+
+}
---
+++ src/main/java/ticketaka/mtvs3_final_backend/hall/tree/command/application/service/TicketTreeCommandService.java
@@ -0,0 +1,16 @@
+package ticketaka.mtvs3_final_backend.hall.tree.command.application.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.hall.tree.command.domain.repository.TicketTreeCommandRepository;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class TicketTreeCommandService {
+
+    private final TicketTreeCommandRepository ticketTreeCommandRepository;
+}
