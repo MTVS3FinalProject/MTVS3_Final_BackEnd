baf751e0a98fb7b37f39d4821835d796070f8b96
INUK-ai <arheon2469@gmail.com>
2024-12-04 22:32:07 +0900

    Feat: TicketCustomQueryService - getCustomTicketList

--- src/main/java/ticketaka/mtvs3_final_backend/ticketing/ticket/custom/query/service/TicketCustomQueryService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/ticketing/ticket/custom/query/service/TicketCustomQueryService.java
@@ -4,10 +4,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.ticketing.ticket.custom.query.dto.TicketCustomQueryResponseDTO;
+import ticketaka.mtvs3_final_backend.ticketing.ticket.query.repository.TicketQueryRepository;
+
+import java.util.List;
 
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class TicketCustomQueryService {
+
+    private final TicketQueryRepository ticketQueryRepository;
+
+    public TicketCustomQueryResponseDTO.getCustomTicketListDTO getCustomTicketList(Long memberId) {
+
+        List<TicketCustomQueryResponseDTO.customTicketDTO> customTicketDTOList = ticketQueryRepository.findCustomTicketImage(memberId);
+
+        return new TicketCustomQueryResponseDTO.getCustomTicketListDTO(customTicketDTOList);
+    }
 }
