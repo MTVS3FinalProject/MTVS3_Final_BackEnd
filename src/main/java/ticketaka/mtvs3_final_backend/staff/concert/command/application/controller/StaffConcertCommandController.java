43f33e4b4b8925ee15276448264dd99ad563c4bb
INUK-ai <arheon2469@gmail.com>
2024-12-09 00:01:23 +0900

    Feat: StaffConcertCommandController - createConcert

--- src/main/java/ticketaka/mtvs3_final_backend/staff/concert/command/application/controller/StaffConcertCommandController.java
+++ src/main/java/ticketaka/mtvs3_final_backend/staff/concert/command/application/controller/StaffConcertCommandController.java
@@ -3,8 +3,13 @@ package ticketaka.mtvs3_final_backend.staff.concert.command.application.controll
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ticketaka.mtvs3_final_backend._core.utils.ApiUtils;
+import ticketaka.mtvs3_final_backend.staff.concert.command.application.dto.StaffConcertCommandRequestDTO;
 import ticketaka.mtvs3_final_backend.staff.concert.command.application.service.StaffConcertCommandService;
 
 @Slf4j
@@ -15,4 +20,15 @@ import ticketaka.mtvs3_final_backend.staff.concert.command.application.service.S
 public class StaffConcertCommandController {
 
     private final StaffConcertCommandService staffConcertCommandService;
+
+    /*
+        공연 생성
+     */
+    @PostMapping
+    public ResponseEntity<?> createConcert(@RequestBody StaffConcertCommandRequestDTO.createConcertDTO requestDTO) {
+
+        staffConcertCommandService.createConcert(requestDTO);
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
 }
