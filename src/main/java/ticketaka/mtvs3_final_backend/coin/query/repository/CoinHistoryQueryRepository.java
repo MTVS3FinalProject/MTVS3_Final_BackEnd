c8854070c56550fb9cbe999a7567ef30dc303080
INUK-ai <arheon2469@gmail.com>
2024-12-14 00:51:33 +0900

    Feat: CoinHistoryQueryRepository - findByMemberIdAndAcquisitionTypeAndCoinAcquisitionId

--- src/main/java/ticketaka/mtvs3_final_backend/coin/query/repository/CoinHistoryQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/coin/query/repository/CoinHistoryQueryRepository.java
@@ -2,8 +2,13 @@ package ticketaka.mtvs3_final_backend.coin.query.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ticketaka.mtvs3_final_backend.coin.command.domain.model.AcquisitionType;
 import ticketaka.mtvs3_final_backend.coin.command.domain.model.CoinHistory;
+
+import java.util.Optional;
 
 @Repository
 public interface CoinHistoryQueryRepository extends JpaRepository<CoinHistory, Long> {
+
+    Optional<CoinHistory> findByMemberIdAndAcquisitionTypeAndCoinAcquisitionId(Long id, AcquisitionType acquisitionType, Long seatId);
 }
