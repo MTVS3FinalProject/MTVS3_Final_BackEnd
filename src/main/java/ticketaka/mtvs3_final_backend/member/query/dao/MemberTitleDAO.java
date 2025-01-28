295bc0732aea8f565c61e51716236fe7ba560689
INUK-ai <arheon2469@gmail.com>
2025-01-29 05:29:18 +0900

    Refactor: MemberTitleDAO - titleList

--- src/main/java/ticketaka/mtvs3_final_backend/member/query/dao/MemberTitleDAO.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/query/dao/MemberTitleDAO.java
@@ -1,12 +1,15 @@
 package ticketaka.mtvs3_final_backend.member.query.dao;
 
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
+import java.util.List;
+
+@Data
 @NoArgsConstructor
 @Document(collection = "member_title")
 public class MemberTitleDAO {
@@ -16,10 +19,29 @@ public class MemberTitleDAO {
 
     @Indexed
     private Long memberId;
-    private Long titleId;
 
-    private String titleName;
-    private String titleScript;
-    private String titleRarity;
-    private Boolean isRepresentative;
+    private List<Title> titleList = List.of();
+
+    @Data
+    @NoArgsConstructor
+    public static class Title {
+        private Long titleId;
+        private String titleName;
+        private String titleScript;
+        private String titleRarity;
+
+        @Builder
+        public Title(Long titleId, String titleName, String titleScript, String titleRarity) {
+            this.titleId = titleId;
+            this.titleName = titleName;
+            this.titleScript = titleScript;
+            this.titleRarity = titleRarity;
+        }
+    }
+
+    @Builder
+    public MemberTitleDAO(Long memberId, List<Title> titleList) {
+        this.memberId = memberId;
+        this.titleList = titleList;
+    }
 }
