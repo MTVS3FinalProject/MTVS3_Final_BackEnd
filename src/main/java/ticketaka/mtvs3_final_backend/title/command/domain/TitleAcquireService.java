82ad8451b47e9276eb2e5d7eaf73fca8c91872b5
INUK-ai <arheon2469@gmail.com>
2025-02-06 04:48:25 +0900

    Feat: getPuzzleResult - event 발행

--- src/main/java/ticketaka/mtvs3_final_backend/title/command/domain/TitleAcquireService.java
+++ src/main/java/ticketaka/mtvs3_final_backend/title/command/domain/TitleAcquireService.java
@@ -7,15 +7,17 @@ import org.springframework.transaction.annotation.Transactional;
 import ticketaka.mtvs3_final_backend.title.command.domain.model.Title;
 import ticketaka.mtvs3_final_backend.title.command.domain.model.TitleRarity;
 import ticketaka.mtvs3_final_backend.title.command.domain.repository.TitleCommandRepository;
+import ticketaka.mtvs3_final_backend.title.command.infrastructure.event.TitleAcquiredEvent;
+import ticketaka.mtvs3_final_backend.title.command.infrastructure.event.TitleEventProducer;
 
 
 @Slf4j
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class TitleAcquireService {
 
     private final TitleCommandRepository titleCommandRepository;
+    private final TitleEventProducer titleEventProducer;
 
     // Title 할당
     public Title getPuzzleResult(Long memberId, Long concertId, TitleRarity titleRarity) {
@@ -23,6 +25,9 @@ public class TitleAcquireService {
         Title title =  titleCommandRepository.getPuzzleResultByMemberIdAndConcertId(memberId, concertId, titleRarity)
                 .orElse(null);
 
+        if (title != null) {
+            titleEventProducer.produceTitleAcquiredEvent(new TitleAcquiredEvent(memberId, title));
+        }
         return title;
     }
 }
