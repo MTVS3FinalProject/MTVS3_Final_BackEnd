5c4aeb582add0709870738fdb55f2d7ffb06f5f6
INUK-ai <arheon2469@gmail.com>
2025-02-06 06:30:08 +0900

    Feat: TitleEventConsumer - consumeTitleAcquiredEvent

--- src/main/java/ticketaka/mtvs3_final_backend/member/query/infrastructure/event/TitleEventConsumer.java
+++ src/main/java/ticketaka/mtvs3_final_backend/member/query/infrastructure/event/TitleEventConsumer.java
@@ -2,10 +2,23 @@ package ticketaka.mtvs3_final_backend.member.query.infrastructure.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ticketaka.mtvs3_final_backend.member.query.service.MemberTitleAcquiredService;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class TitleEventConsumer {
+
+    private final MemberTitleAcquiredService memberTitleAcquiredService;
+
+    @KafkaListener(topics = "title-acquired-event", groupId = "title-query-group")
+    @Transactional
+    public void consumeTitleAcquiredEvent(TitleAcquiredEvent event) {
+        memberTitleAcquiredService.handleTitleAcquired(
+                event.memberId(), event.titleId(), event.titleName(), event.titleScript(), event.titleRarity()
+        );
+    }
 }
