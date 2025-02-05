1e5ba3f7ccd8551e16c5e856d011c50580341fa8
INUK-ai <arheon2469@gmail.com>
2025-02-06 04:47:38 +0900

    Feat: TitleAcquiredEvent - Query

---
+++ src/main/java/ticketaka/mtvs3_final_backend/title/query/infrastructure/event/TitleAcquiredEvent.java
@@ -0,0 +1,15 @@
+package ticketaka.mtvs3_final_backend.title.query.infrastructure.event;
+
+import ticketaka.mtvs3_final_backend.title.command.domain.model.Title;
+
+public record TitleAcquiredEvent(
+        Long memberId,
+        Long titleId,
+        String titleName,
+        String titleScript,
+        String titleRarity
+) {
+    public TitleAcquiredEvent(Long memberId, Title title) {
+        this(memberId, title.getId(), title.getTitleName(), title.getTitleScript(), title.getTitleRarity().toString());
+    }
+}
