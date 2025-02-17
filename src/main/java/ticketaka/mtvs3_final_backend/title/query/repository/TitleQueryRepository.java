ffce7f05439f2027e9c5062acad5626508dbb79c
INUK-ai <arheon2469@gmail.com>
2025-02-17 22:36:30 +0900

    Refactor: TitleQueryRepository - findAllByMemberId 삭제

--- src/main/java/ticketaka/mtvs3_final_backend/title/query/repository/TitleQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/title/query/repository/TitleQueryRepository.java
@@ -9,6 +9,7 @@ import ticketaka.mtvs3_final_backend.member.query.dto.getMemberTitleDTO;
 import ticketaka.mtvs3_final_backend.title.command.domain.model.Title;
 import ticketaka.mtvs3_final_backend.title.command.domain.model.TitleRarity;
 import ticketaka.mtvs3_final_backend.title.command.domain.model.TitleType;
+import ticketaka.mtvs3_final_backend.title.member.command.domain.model.MemberTitle;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,11 +18,4 @@ import java.util.Optional;
 public interface TitleQueryRepository extends JpaRepository<Title, Long> {
 
     List<Title> findAllByTitleTypeAndConcertIdAndTitleRarity(TitleType titleType, Long concertId, TitleRarity titleRarity);
-
-    @Query("SELECT new ticketaka.mtvs3_final_backend.member.query.dto.getMemberTitleDTO(" +
-            "t.id, t.titleName, t.titleScript, t.titleRarity, mt.isRepresentative) " +
-            "FROM Title t " +
-            "JOIN MemberTitle mt ON t.id = mt.titleId " +
-            "WHERE mt.memberId = :memberId")
-    List<getMemberTitleDTO> findAllByMemberId(@Param("memberId") Long memberId);
 }
