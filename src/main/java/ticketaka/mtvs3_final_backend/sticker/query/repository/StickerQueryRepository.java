7f18255b664a6f8712689eec890a06e6b9515af8
INUK-ai <arheon2469@gmail.com>
2025-03-28 04:07:51 +0900

    Refactor: StickerQueryRepository - 미사용 메소드 삭제

--- src/main/java/ticketaka/mtvs3_final_backend/sticker/query/repository/StickerQueryRepository.java
+++ src/main/java/ticketaka/mtvs3_final_backend/sticker/query/repository/StickerQueryRepository.java
@@ -18,27 +18,7 @@ import java.util.List;
 @Repository
 public interface StickerQueryRepository extends JpaRepository<Sticker, Long> {
 
-    List<Sticker> findAllByConcertIdAndStickerType(Long concertId, StickerType stickerType);
-
-    @Query("SELECT s FROM Sticker s " +
-            "JOIN MemberSticker ms ON s.id = ms.stickerId " +
-            "WHERE ms.memberId = :memberId " +
-            "AND s.stickerType = :stickerType")
-    List<Sticker> findAllByMemberIdAndStickerType(@Param("memberId") Long memberId,
-                                                  @Param("stickerType") StickerType stickerType);
-
     List<Sticker> findAllByConcertIdAndStickerTypeAndStickerRarity(Long concertId, StickerType stickerType, StickerRarity stickerRarity);
-
-    @Query("SELECT new ticketaka.mtvs3_final_backend.member.query.dto.getMemberStickerDTO(" +
-            "s.id, s.stickerName, s.stickerScript, s.stickerRarity, f.fileUrl) " +
-            "FROM Sticker s " +
-            "JOIN MemberSticker ms ON s.id = ms.stickerId " +
-            "LEFT JOIN File f ON f.relationType = :relationType AND f.relationId = s.id " +
-            "WHERE ms.memberId = :memberId AND s.stickerType = :stickerType")
-    List<getMemberStickerDTO> findAllByMemberIdAndStickerTypeAndRelationType(@Param("memberId") Long memberId,
-                                                                             @Param("stickerType") StickerType stickerType,
-                                                                             @Param("relationType") RelationType relationType);
-
 
     @Query("SELECT new ticketaka.mtvs3_final_backend.ticketing.ticket.query.dto.stickerDTO(" +
             "s.id, f.fileUrl) " +
